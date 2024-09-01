8692a01ec955a4dd76e3c4bc43049857253942d8
joselopez <alberto.esteva88@gmail.com>
2024-09-01 09:07:56 -0600

    Removing the class name from static methods to perform static imports

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -12,10 +12,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.UUID;
-
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.core.Is.is;
 // controllers in tag stand for the controllers being tested, if no one is assigned all controller will bring for test
 @WebMvcTest(controllers = BeerController.class)
 class BeerControllerTest {
@@ -33,15 +34,15 @@ class BeerControllerTest {
     @Test
     void getBeerById() throws Exception {
         Beer testBeer = beerServiceImpl.listBeers().get(0);
-        BDDMockito.given(beerService.getBeerById(testBeer.getId())).willReturn(testBeer);
+        given(beerService.getBeerById(testBeer.getId())).willReturn(testBeer);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beer/" + testBeer.getId())
+        mockMvc.perform(get("/api/v1/beer/" + testBeer.getId())
                 .accept(MediaType.APPLICATION_JSON)) // Setting that the request accept a Json
-                .andExpect(MockMvcResultMatchers.status().isOk()) // Setting the response Status Ok for all responses
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))// Setting a body type Json for the ResponseEntity
-                //.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(testBeer.getId().toString())) // This is a way to verify if the id of request is equals to id of testBeer
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(testBeer.getId().toString())))// This other way of verify the beer id
-                .andExpect(MockMvcResultMatchers.jsonPath("$.beerName",Matchers.is(testBeer.getBeerName()))) // Verify that beerName in json is equals to testBeer.getBeerName()                                                                                              //According to chatgpt this is a better option to make the comparison
+                .andExpect(status().isOk()) // Setting the response Status Ok for all responses
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))// Setting a body type Json for the ResponseEntity
+                //.andExpect(jsonPath("$.id").value(testBeer.getId().toString())) // This is a way to verify if the id of request is equals to id of testBeer
+                .andExpect(jsonPath("$.id", is(testBeer.getId().toString())))// This other way of verify the beer id
+                .andExpect(jsonPath("$.beerName",is(testBeer.getBeerName()))) // Verify that beerName in json is equals to testBeer.getBeerName()                                                                                              //According to chatgpt this is a better option to make the comparison
         ;
 
     }
