ae112627a109cac37b249b4debc1f8b135848c9f
joselopez <alberto.esteva88@gmail.com>
2024-09-20 23:01:47 -0600

    Happy and Fail Test getById for BeerControllerIntegrationTest

--- src/main/java/guru/springframework/spring6restmvc/controller/NotFoundException.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/NotFoundException.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class NotFoundException extends RuntimeException {
 
     public NotFoundException() {
+        super("Value Not Found");
     }
 
     public NotFoundException(String message) {
         super(message);
     }
+
 
     public NotFoundException(String message, Throwable cause) {
         super(message, cause);
--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIntegrationTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIntegrationTest.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6restmvc.controller;
 
+import guru.springframework.spring6restmvc.entity.Beer;
 import guru.springframework.spring6restmvc.model.BeerDTO;
 import guru.springframework.spring6restmvc.repository.BeerRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,8 +11,11 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class BeerControllerIntegrationTest {
@@ -20,6 +24,22 @@ class BeerControllerIntegrationTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testGetByIdNotFound(){
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> {
+            beerController.getBeerById(UUID.randomUUID());
+        });
+        assertEquals("Value Not Found", exception.getMessage());
+    }
+
+    @Test
+    void testGetById(){
+        Beer beer = beerRepository.findAll().get(0);
+        BeerDTO beerDTO = beerController.getBeerById(beer.getId());
+        assertThat(beerDTO).isNotNull();
+
+    }
 
     @Test
     void testListBeers() {
