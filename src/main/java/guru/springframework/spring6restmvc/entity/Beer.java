e94b53faf1ffe2b4b70d015459ee1052fbdd8a8c
joselopez <alberto.esteva88@gmail.com>
2024-09-22 22:47:14 -0600

    Adding constraints to Beer entity, and updating test

--- src/main/java/guru/springframework/spring6restmvc/entity/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/entity/Beer.java
@@ -2,6 +2,8 @@ package guru.springframework.spring6restmvc.entity;
 
 import guru.springframework.spring6restmvc.model.BeerStyle;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
@@ -26,10 +28,16 @@ public class Beer {
     private UUID id;
     @Version
     private Integer version;
+    @NotBlank
+    @NotNull
     private String beerName;
+    @NotNull
     private BeerStyle beerStyle;
+    @NotNull
+    @NotBlank
     private String upc;
     private Integer quantityOnHand;
+    @NotNull
     private BigDecimal price;
     private LocalDate createdDate;
     private LocalDate updatedDate;
--- src/test/java/guru/springframework/spring6restmvc/repository/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repository/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package guru.springframework.spring6restmvc.repository;
 
 import guru.springframework.spring6restmvc.entity.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,7 +22,15 @@ class BeerRepositoryTest {
     void testSaveBeer(){
         Beer savedBeer = beerRepository.save(Beer.builder()
                         .beerName("My Beer")
+                        .beerStyle(BeerStyle.ALE)
+                        .upc("123456789")
+                        .price(BigDecimal.valueOf(11.99))
                 .build());
+        // If your entity Beer has database-level constraints (e.g., non-null fields, unique constraints),
+        // they won't be enforced until the SQL INSERT statement is actually sent to the database.
+        // If you don't call flush(), Hibernate will batch the SQL statements and execute them only
+        // when the transaction commits, which may be after your test assertions.
+        beerRepository.flush();
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
     }
