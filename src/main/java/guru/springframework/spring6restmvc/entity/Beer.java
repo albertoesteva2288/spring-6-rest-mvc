9b819126c1ac52b0e2a4871e256b31077a43e883
joselopez <alberto.esteva88@gmail.com>
2024-09-24 09:57:58 -0600

    Fixing problems to save the String value of UUID in mysql

--- src/main/java/guru/springframework/spring6restmvc/entity/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/entity/Beer.java
@@ -6,7 +6,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -25,7 +27,8 @@ public class Beer {
     //@GenericGenerator(name = "UUID" , strategy = "org.hibernate.id.UUIDGenerator")
     // Strategy was mark as deprecated, I modified the code of course to the next
     @UuidGenerator
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
     @Version
     private Integer version;
--- src/main/java/guru/springframework/spring6restmvc/entity/Customer.java
+++ src/main/java/guru/springframework/spring6restmvc/entity/Customer.java
@@ -5,7 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -23,7 +25,14 @@ public class Customer {
     //@GenericGenerator(name = "UUID" , strategy = "org.hibernate.id.UUIDGenerator")
     // Strategy was mark as deprecated, I modified the code of course to the next
     @UuidGenerator
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    // UUID was not handled as varchar by default by Hibernate
+    // so that Mysql could save it as such @JdbcTypeCode(SqlTypes.VARCHAR) was used
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    // Hibernate did not recognize the length of the varchar,
+    // even though length = 36 was defined in @Column
+    // so columnDefinition was changed to varchar(36)
+    // this is because id is a UUID type
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
     @NotBlank
     @NotNull
