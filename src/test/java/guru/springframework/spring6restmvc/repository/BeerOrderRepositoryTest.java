05f0e8c2d8dd9173d2d361f23f1f4ba8d33e7f06
joselopez <alberto.esteva88@gmail.com>
2024-10-18 00:52:24 -0600

    Creating a BeerOrderRepository and test

---
+++ src/test/java/guru/springframework/spring6restmvc/repository/BeerOrderRepositoryTest.java
@@ -0,0 +1,41 @@
+package guru.springframework.spring6restmvc.repository;
+
+import guru.springframework.spring6restmvc.entity.Beer;
+import guru.springframework.spring6restmvc.entity.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@DataJpaTest
+@SpringBootTest
+class BeerOrderRepositoryTest {
+
+    @Autowired
+    BeerOrderRepository beerOrderRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    Customer testCustomer;
+    Beer testBeer;
+
+    @BeforeEach
+    void setUp() {
+        testCustomer = customerRepository.findAll().get(0);
+        testBeer = beerRepository.findAll().get(0);
+    }
+    @Test
+    void testBeerOrders(){
+        System.out.println(beerOrderRepository.count());
+        System.out.println(customerRepository.count());
+        System.out.println(beerRepository.count());
+        System.out.println(testCustomer.getCustomerName());
+        System.out.println(testBeer.getBeerName());
+    }
+
+}
