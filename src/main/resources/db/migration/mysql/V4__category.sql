82ea3aaf2cf2c436e96a621b90004981b287a57c
joselopez <alberto.esteva88@gmail.com>
2024-10-19 10:09:04 -0600

    Creating script for category and beer_category tables

---
+++ src/main/resources/db/migration/mysql/V4__category.sql
@@ -0,0 +1,19 @@
+drop table if exists category;
+drop table if exists beer_category;
+
+create table category(
+    id               varchar(36) not null,
+    description      varchar(50),
+    version          integer,
+    created_date     datetime(6),
+    updated_date     datetime(6),
+    primary key (id)
+) ENGINE = InnoDB;
+
+create table beer_category(
+    beer_id          varchar(36) not null,
+    category_id      varchar(36) not null,
+    primary key (beer_id, category_id),
+    constraint pc_beer_id_fk foreign key (beer_id) references beer(id),
+    constraint pc_category_id_fk foreign key (category_id) references category(id)
+) ENGINE = InnoDB;
